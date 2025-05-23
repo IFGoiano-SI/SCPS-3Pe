752804e8ce2936927b4f95f3cab32a1d4e6dc54a
EleniltonFNS <eleniltonfns3@gmail.com>
2025-05-23 19:14:52 -0300

    Inclusão do SQL do Banco de Dados

---
+++ DB/BD_SCPS.sql
@@ -0,0 +1,232 @@
+CREATE DATABASE  IF NOT EXISTS `qualha64_bd_scps` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+USE `qualha64_bd_scps`;
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: br1030.hostgator.com.br    Database: qualha64_bd_scps
+-- ------------------------------------------------------
+-- Server version	5.7.23-23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telefone` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `id_endereco` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `ativo` tinyint(4) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id_cliente`),
+  KEY `id_endereco` (`id_endereco`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente`
+--
+
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` VALUES (4,'Rubiataba Industrial','62996956448','elenilton.silva@rubisa.com.br',11,'2025-05-08 20:26:46','2025-05-09 19:28:54',1),(5,'CRV industrial','62999344425','crvindustrial@crvindustrial.com',12,'2025-05-09 19:23:57','2025-05-09 20:38:25',0);
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `endereco`
+--
+
+DROP TABLE IF EXISTS `endereco`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `endereco` (
+  `id_endereco` int(11) NOT NULL AUTO_INCREMENT,
+  `rua` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `numero` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `bairro` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cidade` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `estado` char(2) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cep` char(9) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_endereco`),
+  KEY `cidade` (`cidade`),
+  KEY `estado` (`estado`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `endereco`
+--
+
+LOCK TABLES `endereco` WRITE;
+/*!40000 ALTER TABLE `endereco` DISABLE KEYS */;
+INSERT INTO `endereco` VALUES (8,'rtyui','ytyuio','tyu','rtyu','GO','6789','2025-05-08 20:11:31','2025-05-08 20:11:31'),(9,'1234','1234','123','123','go','1234','2025-05-08 20:14:11','2025-05-08 20:14:11'),(10,'Rua das1','123','teste','Rubiataba','GO','76350000','2025-05-08 20:21:37','2025-05-08 20:21:37'),(11,'Algum Local','sn','Zona Rural','Rubiataba','Go','763500000','2025-05-08 20:26:40','2025-05-09 19:28:51'),(12,'Zona Rural','sn','GO','Carmo do Rio Verde','GO','76340000','2025-05-09 19:23:54','2025-05-09 19:23:54'),(13,'rt','rty','rty','fg','go','763400000','2025-05-09 19:30:14','2025-05-09 19:30:14'),(14,'Algum local','sn','Não sei','Itapaci','Go','76360000','2025-05-09 19:35:05','2025-05-09 19:35:05'),(15,'Alguma de Ceres','sn','Não sei','Rialma','GO','76350000','2025-05-09 20:09:14','2025-05-09 20:09:14'),(16,'Sem','sn','Algum ai','Itapaci','GO','76350000','2025-05-09 20:16:37','2025-05-09 20:16:37'),(17,'alguma rua','sn','Jardim Sorriso','Itapaci','Go','76123000','2025-05-09 20:24:11','2025-05-09 20:24:11'),(18,'zsdfgth','sdfg','dfg','sdfg','go','342334','2025-05-09 20:31:19','2025-05-09 20:31:19');
+/*!40000 ALTER TABLE `endereco` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `funcionario`
+--
+
+DROP TABLE IF EXISTS `funcionario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `funcionario` (
+  `id_funcionario` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cargo` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telefone` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `id_endereco` int(11) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `ativo` tinyint(4) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id_funcionario`),
+  UNIQUE KEY `email` (`email`),
+  KEY `id_endereco` (`id_endereco`),
+  KEY `id_usuario` (`id_usuario`),
+  CONSTRAINT `funcionario_ibfk_1` FOREIGN KEY (`id_endereco`) REFERENCES `endereco` (`id_endereco`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `funcionario_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `funcionario`
+--
+
+LOCK TABLES `funcionario` WRITE;
+/*!40000 ALTER TABLE `funcionario` DISABLE KEYS */;
+INSERT INTO `funcionario` VALUES (2,'Elenilton Silva','Gerente','62999344425','elenilton.silva@scps.com.br',8,4,'2025-05-08 20:11:49','2025-05-09 20:21:34',1),(3,'Iasmyn Severo','Assistente de Financeiro','62999998888','iasmyn.severo@scps.com.br',14,5,'2025-05-09 19:35:41','2025-05-09 20:21:33',1),(4,'Giovana Lyssa','Designer','62987654321','giovana.lyssa@scps.com.br',15,6,'2025-05-09 20:09:38','2025-05-23 19:04:24',0),(5,'Janiele Machado','Social Media','6298125683','janiele.machado@scps.com.br',16,7,'2025-05-09 20:16:59','2025-05-09 20:18:50',0),(6,'rfty','sdfg','srdtfygh','fdghj',18,8,'2025-05-09 20:31:30','2025-05-23 17:42:28',0);
+/*!40000 ALTER TABLE `funcionario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ordem_servico`
+--
+
+DROP TABLE IF EXISTS `ordem_servico`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ordem_servico` (
+  `id_ordem` int(11) NOT NULL AUTO_INCREMENT,
+  `data_abertura` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `data_conclusao` datetime DEFAULT NULL,
+  `status` enum('ABERTO','EM_EXECUCAO','CONCLUIDA','CANCELADA') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'ABERTO',
+  `id_cliente` int(11) NOT NULL,
+  `id_funcionario` int(11) NOT NULL,
+  `descricao` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `valor_pago` double DEFAULT '0',
+  PRIMARY KEY (`id_ordem`),
+  KEY `id_cliente` (`id_cliente`),
+  KEY `id_funcionario` (`id_funcionario`),
+  KEY `status` (`status`),
+  CONSTRAINT `ordem_servico_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`) ON UPDATE CASCADE,
+  CONSTRAINT `ordem_servico_ibfk_2` FOREIGN KEY (`id_funcionario`) REFERENCES `funcionario` (`id_funcionario`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ordem_servico`
+--
+
+LOCK TABLES `ordem_servico` WRITE;
+/*!40000 ALTER TABLE `ordem_servico` DISABLE KEYS */;
+INSERT INTO `ordem_servico` VALUES (1,'2025-05-08 21:46:55','2025-05-09 01:20:27','CONCLUIDA',4,2,'Batatinha','2025-05-08 21:47:26','2025-05-09 01:20:33',2010);
+/*!40000 ALTER TABLE `ordem_servico` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `servico`
+--
+
+DROP TABLE IF EXISTS `servico`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `servico` (
+  `id_servico` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descricao` text COLLATE utf8mb4_unicode_ci,
+  `preco` decimal(10,2) NOT NULL,
+  `id_funcionario_responsavel` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `sequencia` int(11) NOT NULL,
+  `id_ordem_servico` int(11) NOT NULL,
+  PRIMARY KEY (`id_servico`),
+  KEY `id_funcionario_responsavel` (`id_funcionario_responsavel`),
+  KEY `nome` (`nome`),
+  KEY `fk_ordem_servico_idx` (`id_ordem_servico`),
+  CONSTRAINT `fk_ordem_servico` FOREIGN KEY (`id_ordem_servico`) REFERENCES `ordem_servico` (`id_ordem`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `servico_ibfk_1` FOREIGN KEY (`id_funcionario_responsavel`) REFERENCES `funcionario` (`id_funcionario`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `servico`
+--
+
+LOCK TABLES `servico` WRITE;
+/*!40000 ALTER TABLE `servico` DISABLE KEYS */;
+/*!40000 ALTER TABLE `servico` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_usuario` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `senha` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tipo_usuario` enum('ADMIN','FUNCIONARIO','INATIVO') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `ativo` tinyint(4) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id_usuario`),
+  UNIQUE KEY `nome_usuario` (`nome_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (4,'eleh','d4d05ffedceb73312ac20626a34f248c90786dd4','ADMIN','2025-05-08 20:11:45','2025-05-09 20:11:05',1),(5,'iasmyn.severo','a68fa14ff288bc54a2da230603aba870930061a6','FUNCIONARIO','2025-05-09 19:35:38','2025-05-09 19:35:38',1),(6,'giovana.lyssa','ac859f658354bb2b3cbf0dad927a0d8aed1c052a','FUNCIONARIO','2025-05-09 20:09:35','2025-05-09 20:09:35',1),(7,'janiele.machado','646c3fad45809c2a958cce7b5df636ef8de93e7d','FUNCIONARIO','2025-05-09 20:16:56','2025-05-09 20:16:56',1),(8,'eu','b7b18c7cc89c1918fa4956fc3cf64fb6dd70d494','FUNCIONARIO','2025-05-09 20:31:26','2025-05-23 19:09:29',0);
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-05-23 19:12:36
